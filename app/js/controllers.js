09c7a9b6baa9f538a2d5bb46e24426b3a794ab7e
bcarter <github@idmsinc.net>
2013-01-04 12:15:43 -0700

    Started pet display page

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,8 +3,24 @@
 /* Controllers */
 
 
-function MyCtrl1() {}
-MyCtrl1.$inject = [];
+function MyCtrl1($scope, $http) {
+                   $scope.testvalue="howdy";
+//    var jpurl = "http://us.battle.net/api/wow/battlePet/ability/640?jsonp=JSON_CALLBACK";
+    var jpurl = "http://us.battle.net/api/wow/character/Azgalor/Delevent?fields=pets&jsonp=JSON_CALLBACK";
+
+    $http.jsonp(jpurl).success(function(data){
+        $scope.pets =  data;
+    });
+
+//    $scope.doRequest = function() {
+//        var jpurl = "http://us.battle.net/api/wow/battlePet/ability/640?jsonp=JSON_CALLBACK";
+//
+//        $http.jsonp(jpurl).success(function(data){
+//            $scope.testJsonp =  data;
+//        });
+//    };
+}
+//MyCtrl1.$inject = [];
 
 
 function MyCtrl2() {
